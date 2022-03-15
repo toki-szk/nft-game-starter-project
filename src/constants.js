c1914bdf062a7f6ec87c613dd016934ef9695375
toki suzuki <tokinoshin.suzuki@pilotz.jp>
2022-03-15 22:20:46 +0900

    [Add] upload to github

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = "0xeEa72ac11BE9F0aACFA448a59987AB476ADcE64C";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
